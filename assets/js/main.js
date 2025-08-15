b82f0edc775d06f6127d84e0293a5be073324b3a
Hiyu Yamauchi <130884957+hiyu15123@users.noreply.github.com>
2025-08-15 11:52:50 +0900

    add : about usを作成。
    about usを作成しました。

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,24 @@
-function triggerBtn() {
+function headerBtn() {
    document.querySelector('.js-trigger-btn').addEventListener('click', function() {
       this.classList.toggle('is-active');
       document.querySelector('.js-target-btn').classList.toggle('is-active');
    });
 }
 
-
+function aboutHoverTrigger() {
+   const trigger = document.querySelectorAll(".js-about-trigger li a");
+   const target = document.querySelectorAll(".js-about-target li");
+   for (let i = 0; i < trigger.length; i++) {
+      trigger[i].addEventListener("mouseover", function() {
+         for (let j = 0;j < target.length; j++) {
+            target[j].classList.remove("is-active");
+         }
+         target[i].classList.add("is-active");
+      })
+   }
+}
 
 window.addEventListener('DOMContentLoaded', function() {
-   triggerBtn();
+   headerBtn();
+   aboutHoverTrigger()
 });
